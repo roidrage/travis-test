5fd2e3ba78371a43c3a781e9e8f6c9818adda3fa
Mathias Meyer <meyer@paperplanes.de>
2013-03-20 21:42:20 +0100

    Change args to really fail.

--- args.py
+++ args.py
@@ -4,4 +4,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help = "Build file",
 metavar = "file", default = "build.py")
 
-parser.parse_args("[--file]") # this line should throw SystemExit
+parser.parse_args(["--file"]) # this line should throw SystemExit
